c02d7b45bf32bd7acd2f350db5d243e8b094fb47
a <a>
2021-09-05 10:22:49 +0900

    update

--- tool/startup.sh
+++ tool/startup.sh
@@ -4,8 +4,6 @@ DIR=$(cd $(dirname $0); pwd)
 HOME_DIR=${DIR}/..
 
 pushd ${DIR}
-  # git clone https://github.com/guri10gura/dotfiles
-  git clone ~/work/dotfiles/.git
   git clone https://github.com/guri10gura/dotfiles
   cd ${HOME_DIR}
   rm -r ./tool/dotfiles/.git
